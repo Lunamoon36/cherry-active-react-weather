49e4d98f2bee2e151c40f6e5f9182b1832a8f337
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-05-24 21:03:02 +0100

    Added the footer and links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1><footer>
+        This project was coded by
+        <a
+          href="https://github.com/Lunamoon36"
+          class="myProfileLink"
+          target="_blank"
+          rel='noreferrer'
+          ><strong> Cherry Atkinson </strong></a
+        >
+        and is on
+        <a
+          href="https://github.com/Lunamoon36/cherry-active-react-weather"
+          class="myGitHubLink"
+          target="_blank"
+          rel='noreferrer'
+          ><strong> GitHub </strong></a
+        >
+        and hosted on
+        <a
+          href="https://weathercherryplus.netlify.app/"
+          class="myNetlifyLink"
+          target="_blank"
+          rel='noreferrer'
+          ><strong> Netlify </strong></a
+        >
+      </footer>
     </div>
   );
 }
